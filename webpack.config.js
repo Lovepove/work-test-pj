0eebac7708744b9dd08762b75d55ea51bf4ad3cf
Lovepove <lovepove@gmail.com>
2018-11-10 16:19:58 +0100

    inital setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+      path.join(__dirname, 'src'),
+    ],
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+        {
+          from: 'src/css/main.css',
+          to: 'main.css',
+          toType: 'file'
+        }, {
+            from: 'src/html/index.html',
+            to: 'index.html',
+            toType: 'file'
+        }
+      ]),
+    new WriteFilePlugin()    
+  ],
+}
